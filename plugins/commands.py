b203ea4b9f54190fb95662d1c33ad40ddc426012
SMILE-KILLER10 <85184491+SMILE-KILLER10@users.noreply.github.com>
2021-06-15 18:46:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("MASTER⚓️", url="https://youtu.be/hkmc3e7U7R4"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("💞Channel💞", url="https://t.me/publicchannalin"),
+                    InlineKeyboardButton("💘Group💘", url="https://t.me/filmcornermm")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/SMILE-KILLER10/Fc-TOVINO")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
